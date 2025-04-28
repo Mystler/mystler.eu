422764261f345f4247978e26306e598cc07e8eaa
Florian Meißner <developer@mystler.eu>
2025-04-28 16:46:32 +0200

    feat: Header transition on mobile

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+  import { quintOut } from "svelte/easing";
+  import { slide } from "svelte/transition";
+
   let menuOpen: boolean = $state(false);
 
   function toggleMenu(event: MouseEvent) {
@@ -14,6 +17,14 @@
   }
 </script>
 
+{#snippet menu()}
+  <a href="/projects">Projects</a>
+  <a href="/music">Music</a>
+  <a href="/photos">Photos</a>
+  <a href="/words">Words</a>
+  <a href="/about">About</a>
+{/snippet}
+
 <header class="lg:sticky portrait:sticky top-0 z-50">
   <nav class="bg-zinc-900 sm:flex max-sm:divide-y max-sm:divide-zinc-600">
     <div class="flex justify-between px-3">
@@ -24,23 +35,19 @@
         </button>
       </div>
     </div>
-    <div class="menu {menuOpen ? 'block' : 'hidden'}">
-      <a href="/projects">Projects</a>
-      <a href="/music">Music</a>
-      <a href="/photos">Photos</a>
-      <a href="/words">Words</a>
-      <a href="/about">About</a>
+    <div class="menu hidden sm:flex sm:flex-wrap">
+      {@render menu()}
     </div>
+    {#if menuOpen}
+      <div transition:slide={{ easing: quintOut }} class="menu sm:hidden divide-y divide-zinc-700">
+        {@render menu()}
+      </div>
+    {/if}
   </nav>
 </header>
 
 <style lang="postcss">
   @reference "$lib/styles/base.css";
-
-  .menu {
-    @apply sm:flex sm:flex-wrap;
-    @apply max-sm:divide-y max-sm:divide-zinc-700;
-  }
 
   .menu > a {
     @apply block px-4 py-3 hover:bg-zinc-700;
