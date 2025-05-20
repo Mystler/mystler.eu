37b8271227e835f39f287a2cc5c68e939ba4df18
Florian Meißner <developer@mystler.eu>
2025-05-20 14:33:24 +0200

    chore: Update dependencies Includes path fix for mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,12 @@ import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import { mdsvex } from "mdsvex";
 import gfm from "remark-gfm";
+import { fileURLToPath } from "url";
+import { join, dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const mdLayout = join(__dirname, "./src/lib/components/MarkdownContent.svelte");
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,7 +16,7 @@ const config = {
     mdsvex({
       extensions: [".md"],
       remarkPlugins: [gfm],
-      layout: "./src/lib/components/MarkdownContent.svelte",
+      layout: mdLayout,
     }),
   ],
   extensions: [".svelte", ".md"],
